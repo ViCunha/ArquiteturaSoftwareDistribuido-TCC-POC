7b505c9048b6182811425c716ee7af4d6a48003c
Vinicius Martins Cunha <vicunha@outlook.com>
2021-09-18 12:26:02 -0300

    Identity.Domain > Add 'Model' folder > Add a class named: 'Entity'

---
+++ src/Identity.Domain/Model/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Domain.Model
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+    }
+}
