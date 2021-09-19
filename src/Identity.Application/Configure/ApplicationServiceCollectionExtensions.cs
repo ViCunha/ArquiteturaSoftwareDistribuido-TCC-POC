2f0aa61300ab4365134962eaa4855c84afad829f
Vinicius Martins Cunha <vicunha@outlook.com>
2021-09-18 23:05:20 -0300

    Identity.Application > Add 'Configure' folder > Add a class 'ApplicationServiceCollectionExtensions';

---
+++ src/Identity.Application/Configure/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Application.Configure
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServiceCollection(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
