a8344b557b2db4b20c8f43b5330757d0ad7eab30
Vinicius Martins Cunha <vicunha@outlook.com>
2021-09-19 23:53:48 -0300

    Identity.Infrastructure.Persistence > Add 'Repositories' folder > Add RepositoryCustomer class and extract IRepositoryCustomer interface; Update RepositoryCustomer

---
+++ src/Identity.Infrastructure.Persistence/Repositories/RepositoryCustomer.cs
@@ -0,0 +1,25 @@
+﻿using Identity.Domain.Interfaces;
+using Identity.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Persistence.Repositories
+{
+    public class RepositoryCustomer : IRepositoryCustomer
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RepositoryCustomer(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllCustomers()
+        {
+            return await _unitOfWork.CustomerRepository.GetAllAsync();
+        }
+    }
+}
