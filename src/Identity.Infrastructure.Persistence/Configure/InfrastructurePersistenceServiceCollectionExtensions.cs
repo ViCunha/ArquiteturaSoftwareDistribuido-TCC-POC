e3873423b27c3722958617b7b5c80e207eb79cfe
Vinicius Martins Cunha <vicunha@outlook.com>
2021-09-19 23:01:29 -0300

    Identity.Infrastructure.Persistence > Add 'Configure' folder > Add a class 'InfrastructurePersistenceServiceCollectionExtensions' > Update 'InfrastructurePersistenceServiceCollectionExtensions';

---
+++ src/Identity.Infrastructure.Persistence/Configure/InfrastructurePersistenceServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Persistence.Configure
+{
+    public static class InfrastructurePersistenceServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructurePersistenceServiceCollection(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
