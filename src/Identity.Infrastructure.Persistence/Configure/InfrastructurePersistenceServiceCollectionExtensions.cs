a28dd666b51f0ce87401412ff07c296b7d350915
Vinicius Martins Cunha <vicunha@outlook.com>
2021-09-19 23:27:57 -0300

    Identity.Infrastructure.Persistence > Update 'ApplicationDBContext' class;  Add reference to 'Identity.Domain' project

--- src/Identity.Infrastructure.Persistence/Configure/InfrastructurePersistenceServiceCollectionExtensions.cs
+++ src/Identity.Infrastructure.Persistence/Configure/InfrastructurePersistenceServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Identity.Infrastructure.Persistence.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +12,14 @@ namespace Identity.Infrastructure.Persistence.Configure
 {
     public static class InfrastructurePersistenceServiceCollectionExtensions
     {
-        public static IServiceCollection AddInfrastructurePersistenceServiceCollection(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructurePersistenceServiceCollection(this IServiceCollection services, IConfiguration configuration)
         {
+
+            // Dependecy Injection 
+            {
+                services.AddDbContext<ApplicationDBContextQueries>
+                    (options => options.UseSqlServer(configuration.GetConnectionString("ApplicationDbContextQueries")));
+            }
 
             return services;
         }
---
+++ src/Identity.Infrastructure.Persistence/DBContext/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Identity.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Persistence.DBContext
+{
+    public class ApplicationDBContextQueries : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+
+        public ApplicationDBContextQueries(DbContextOptions<ApplicationDBContextQueries> options)
+            : base(options)
+        {
+
+        }
+    }
+}
