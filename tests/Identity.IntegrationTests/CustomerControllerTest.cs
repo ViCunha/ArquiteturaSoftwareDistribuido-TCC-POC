f3e4e9c70dc817b20e3bfcb3ef3cd499340c78e4
Vinicius Martins Cunha <vicunha@outlook.com>
2021-09-25 23:16:12 -0300

    Identity.IntegrationTests > Rename 'CustomerController_GetAllCustomers_ExpectedResultOkAndCollectionOfObjects' to 'CustomerController_GetAllCustomers_ExpectedResultOkAndCollectionOfCustomerObjects'

--- tests/Identity.IntegrationTests/CustomerControllerTest.cs
+++ tests/Identity.IntegrationTests/CustomerControllerTest.cs
@@ -15,7 +15,7 @@ namespace Identity.IntegrationTests
     public class CustomerControllerTest
     {
         [Fact]
-        public async Task CustomerController_GetAllCustomers_ExpectedResultOkAndCollectionOfObjects()
+        public async Task CustomerController_GetAllCustomers_ExpectedResultOkAndCollectionOfCustomerObjects()
         {
             //Arrange
             var mocker = new AutoMocker();
@@ -55,5 +55,48 @@ namespace Identity.IntegrationTests
                     );
             }
         }
+
+        [Fact]
+        public async Task CustomerController_CreateNewCustomer_ExpectedResultOkAndCustomerObject()
+        {
+            //Arrange
+            var mocker = new AutoMocker();
+            var controller = mocker.CreateInstance<CustomerController>();
+            mocker.GetMock<IGetAllCustomersQuery>()
+                .Setup(x => x.GetAllCustomers())
+                .Returns(GetAllCustomers());
+
+            //Act
+            var response = await controller.GetAllCustomers();
+            var statuscodeOk200Result = response.Result as OkObjectResult;
+            var resultObject = GetCollectionResultContent<IEnumerable<Customer>>(statuscodeOk200Result);
+
+            //Assert
+            Assert.True(statuscodeOk200Result != null);
+            mocker.GetMock<IGetAllCustomersQuery>().Verify(c => c.GetAllCustomers(), Times.Once);
+            Assert.True(resultObject.Result.Count() == 3);
+
+            async static Task<T> GetCollectionResultContent<T>(ActionResult<T> result)
+            {
+                await Task.Factory.StartNew(() => { });
+                return (T)((ObjectResult)result.Result).Value;
+            }
+
+            async static Task<IEnumerable<Customer>> GetAllCustomers()
+            {
+                return await Task.Factory.StartNew
+                (
+                    () => new List<Customer>()
+                    {
+                        new Customer() { Id = Guid.NewGuid(), isActive = true, Name = "a" }
+                        ,
+                        new Customer() { Id = Guid.NewGuid(), isActive = true, Name = "b" }
+                        ,
+                        new Customer() { Id = Guid.NewGuid(), isActive = true, Name = "c" }
+                    }
+                );
+            }
+        }
+
     }
 }
