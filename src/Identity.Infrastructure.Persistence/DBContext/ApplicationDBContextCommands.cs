1b39933fda30a1dfd700fd028bebb8a8b32b4321
Vinicius Martins Cunha <vicunha@outlook.com>
2021-09-25 22:25:26 -0300

    Identity.Infrastructure.Persistence > Add ApplicationDBContextCommands class to ~DBContext

---
+++ src/Identity.Infrastructure.Persistence/DBContext/ApplicationDBContextCommands.cs
@@ -0,0 +1,21 @@
+﻿using Identity.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Persistence.DBContext
+{
+    public class ApplicationDBContextCommands : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public ApplicationDBContextCommands(DbContextOptions<ApplicationDBContextCommands> options)
+            : base(options)
+        {
+
+        }
+    }
+}
