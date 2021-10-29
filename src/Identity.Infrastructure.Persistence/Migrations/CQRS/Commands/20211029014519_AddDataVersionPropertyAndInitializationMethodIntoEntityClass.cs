fc3b0c7a13bf559ac5711eb9387ab6cf872b145c
Vinicius Martins Cunha <vicunha@outlook.com>
2021-10-28 22:45:42 -0300

    [Optimistic Concurrency] Add-migration 'AddDataVersionPropertyAndInitializationMethodIntoEntityClass' -Context ApplicationDbContextCommands -Output Migrations/CQRS/Commands

---
+++ src/Identity.Infrastructure.Persistence/Migrations/CQRS/Commands/20211029014519_AddDataVersionPropertyAndInitializationMethodIntoEntityClass.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity.Infrastructure.Persistence.Migrations.CQRS.Commands
+{
+    public partial class AddDataVersionPropertyAndInitializationMethodIntoEntityClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DataVersion",
+                table: "TransactionProcessingControlHistory",
+                type: "decimal(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DataVersion",
+                table: "EventSourcingHistory",
+                type: "decimal(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DataVersion",
+                table: "Customers",
+                type: "decimal(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataVersion",
+                table: "TransactionProcessingControlHistory");
+
+            migrationBuilder.DropColumn(
+                name: "DataVersion",
+                table: "EventSourcingHistory");
+
+            migrationBuilder.DropColumn(
+                name: "DataVersion",
+                table: "Customers");
+        }
+    }
+}
