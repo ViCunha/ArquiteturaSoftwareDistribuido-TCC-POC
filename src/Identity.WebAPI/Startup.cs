1e95f4d8b440675ab1bf3c041662f3273ac11de9
Vinicius Martins Cunha <vicunha@outlook.com>
2021-11-11 00:27:37 -0300

    [JWT] Identity.WebAPI > Partial update ConfigureServices into the Startup class

--- src/Identity.WebAPI/Startup.cs
+++ src/Identity.WebAPI/Startup.cs
@@ -27,6 +27,32 @@ namespace Identity.WebAPI
         {
             services.AddApplicationServiceCollection(Configuration);
 
+            services.AddAuthentication
+                     (
+                        options =>
+                        {
+                            //options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                            //options.DefaultChallengeScheme = 
+                        }
+
+                    );
+                     
+                     //.AddJwtBearer(options =>
+                     //       {
+                     //           options.TokenValidationParameters = new TokenValidationParameters
+                     //           {
+                     //               ValidateIssuer = true,
+                     //               ValidateAudience = true,
+                     //               ValidateLifetime = true,
+                     //               ValidateIssuerSigningKey = true,
+
+                     //               ValidIssuer = Configuration["Jwt:Issuer"],
+                     //               ValidAudience = Configuration["Jwt:Audience"],
+                     //               IssuerSigningKey = new SymmetricSecurityKey
+                     //             (Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                     //           };
+                     //       });
+
             services.AddApiVersioning
                 (
                     options => 
